c7274727578fee9119c1d3fcd250094bbd49e6f8
Ethan-yu-happy <1003795381@qq.com>
2020-05-09 02:25:17 +0800

    First Test

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2020/5/9
+  Time: 0:33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>尚硅谷官网</title>
+  </head>
+  <body>
+  <h1 style="color: #ff4500">俞志航</h1>
+  </body>
+</html>
---
+++ Module1/src/com/atguigu/bean.java
@@ -0,0 +1,77 @@
+package com.atguigu;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+//单行注释
+/*
+    多行注释
+ */
+
+/**
+ *文档注释
+ */
+class Bean {
+    public static void main(String[] args) {
+        System.out.println("helloworld 2");
+        ArrayList a = new ArrayList();
+        Map map=new HashMap<>();
+        test1();
+
+        //        method();
+
+    }
+
+    public static void test1() {
+        //region Description
+        Bean b=new Bean();
+        b.test11();
+        b.test12();
+        b.test13();
+        b.test14();
+        //endregion
+    }
+
+    public static void method(){
+        try {
+            FileInputStream in = new FileInputStream("hello.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void test11(){
+        String str1 ="hello world";
+        String str2 ="hello";
+        String str3 =str2+" world";
+        System.out.println(str1==str3);
+    }
+    public void test12(){
+        String str1 ="hello world";
+        final String str2 ="hello";
+        String str3 =str2+" world";
+        System.out.println(str1==str3);
+    }
+    public void test13(){
+        String str1 ="hello world";
+        final String str2 =new String("hello");
+        String str3 =str2+" world";
+        System.out.println(str1==str3);
+    }
+    public void test14(){
+        String str1 ="hello world";
+        String str2 ="hello";
+        String str3 =" world";
+        String str4 = str2+str3;
+        System.out.println("str2:"+str2.hashCode());
+        System.out.println("str3:"+str3.hashCode());
+        System.out.println("str1:"+str1.hashCode());
+        System.out.println("str4:"+str4.hashCode());
+        System.out.println(str1==str4);
+        System.out.println(str1==str4.intern());
+    }
+
+}
